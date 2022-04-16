a9fa529f7c2e30a4f48914a7a2ee0ed81f5c4a60
cmulero83 <cmulero83@icloud.com>
2022-04-16 14:06:44 -0500

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+// Ejercicio: https://www.youtube.com/watch?v=FPxlb6hEeWA&list=PLHgpVrCyLWAoSkzNPYt9nhmtSlpXjtnju&index=5
+// https://rxjs.dev/api/operators/tap
+// tap, no es un observable comun, permite espiar un observable
+//
+
+import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs_4_tap';
+
+  public ngOnInit(): void{
+     
+    // Aqui llamamos a los clicks del documento, ya tenemos un observable
+    // Este observable no tiene next, complete, etc.
+
+    const clicks = fromEvent(document, 'click')
+
+    // Al utilizar TAP, le añade la posibilidad de usar err, complete, etc...
+    const positions = clicks.pipe(
+      tap( ev => console.log('Procesado ', ev),
+      err => console.log('Err : ' + err),
+      () => console.log('Completado')
+      )
+    )
+
+    positions.subscribe(pos => console.log(pos))
+
+
+  }
 }
